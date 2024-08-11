b220ce824138acbe05cd8889790045f1ff5cb1fc
Samir H <ultrakorne@gmail.com>
2024-08-11 21:29:48 +0200

    day 3 one star

--- bin/day3.ml
+++ bin/day3.ml
@@ -1,27 +1,37 @@
 let day3_inputs = Utils.read_lines "data/advent_3_data.txt"
-let first_line = List.nth day3_inputs 0
-let second_line = List.nth day3_inputs 1
+let previous_line = List.nth day3_inputs 0
+let first_line = List.nth day3_inputs 1
+let second_line = List.nth day3_inputs 2
+
+(*Check if there is a symbol around the index i*)
 
 let check_line line prev_line next_line =
   (*Check around and diagonall, line above and under if there is a symbol around index i*)
   let check_symbol_around i =
     let check_is_symbol str i =
       if i < 0 || i >= String.length str then false
-      else 
-        let () = Printf.printf "Checking at %d: %c\n" i line.[i] in
+      else
+        (* let () = Printf.printf "Checking at %d: %c\n" i line.[i] in *)
         match str.[i] with '0' .. '9' | '.' | ' ' -> false | _ -> true
     in
-    let tc = check_is_symbol prev_line i in
-    let bc = check_is_symbol next_line i in
-    let symbol_found = tc || bc in
-    if symbol_found then
-      Printf.printf "Symbol found at %d around %c\n" i line.[i];
+
+    let check_pos ln =
+      List.exists (fun offset -> check_is_symbol ln (i + offset)) [ -1; 0; 1 ]
+    in
+
+    let symbol_found =
+      check_pos prev_line || check_pos line || check_pos next_line
+    in
+
+    (* if symbol_found then
+      Printf.printf "Symbol found at %d around %c\n" i line.[i]; *)
     symbol_found
   in
 
   (*return a tuple with the string number and the length of the string number if the number is invalid returns 0*)
   let rec str_check ?(valid = false) i str_acc l =
-    if i >= String.length line then ((if valid then str_acc else "0"), l)
+    let result = if valid then str_acc else "0" in
+    if i >= String.length line then (result, l)
     else
       match line.[i] with
       | '0' .. '9' as c ->
@@ -29,18 +39,17 @@ let check_line line prev_line next_line =
           str_check (i + 1)
             (str_acc ^ String.make 1 c)
             (l + 1) ~valid:(valid || symbol_around)
-      | _ -> ((if valid then str_acc else "0"), l)
+      | _ -> (result, l)
   in
 
   let rec aux i acc =
     if i >= String.length line then acc
     else
-      (* let () = Printf.printf "Char: %c\n" line.[i] in *)
       match line.[i] with
-      | '0' .. '9' as c ->
+      | '0' .. '9' ->
           (* if it's a number char, we try to see if there are other numbers after, to return the full*)
-          let str_num, l = str_check (i + 1) (String.make 1 c) 1 in
-          Printf.printf "Str: %s - %d\n" str_num l;
+          let str_num, l = str_check i "" 0 in
+          (* Printf.printf "Str: %s - %d\n" str_num l; *)
           aux (i + l) (acc + int_of_string str_num)
       | '.' -> aux (i + 1) acc
       | _ -> aux (i + 1) acc
@@ -48,6 +57,18 @@ let check_line line prev_line next_line =
   aux 0 0
 
 let execute =
-  let test_val = check_line first_line "" second_line in
+  let rec fold_lines acc current_line prev_line lines =
+    (* let () = Printf.printf "Current line: %s - acc %d\n" current_line acc in *)
+    match lines with
+    | [] -> acc + check_line current_line prev_line ""
+    | next_line :: tl ->
+        let new_acc = acc + check_line current_line prev_line next_line in
+        fold_lines new_acc next_line current_line tl
+  in
+  (* start with line one, prev line is empty and all the rest of the lines*)
+  fold_lines 0 (List.hd day3_inputs) "" (List.tl day3_inputs)
+
+(* let execute =
+  let test_val = check_line first_line previous_line second_line in
   Printf.printf "First line: %d\n" test_val;
-  0
+  0 *)
