69cae092793a57b32cce0931effa18d1fb09b793
Samir H <ultrakorne@gmail.com>
2024-08-23 00:21:10 +0200

    day 9 second star

--- bin/day9.ml
+++ bin/day9.ml
@@ -10,22 +10,31 @@ let find_next_num list =
       match lst with l1 :: l2 :: tail -> sub (l2 :: tail) (sub_lst @ [ l2 - l1 ]) | _ -> sub_lst
     in
 
-    if List.for_all (fun x -> x = 0) lst then 0
-    else
+    if List.exists (fun x -> x <> 0) lst then
       let prev_sub = sub lst [] in
-      (* let () = List.iter (Printf.printf "| p: %d ") prev_sub in *)
-      (* let () = Printf.printf "\n" in *)
       let res = aux prev_sub in
       let return = res + (List.rev prev_sub |> List.hd) in
-      (* let () = Printf.printf "\nreturn val %d " return in *)
       return
+    else 0
   in
   aux list + (List.rev list |> List.hd)
 
-(* let () = List.iter (Printf.printf "\n next: %d ") next_num *)
-let execute () =
-  (* let next_num = find_next_num test_result in *)
-  (* next_num *)
-  List.fold_left (fun acc x -> acc + (parse_line x |> find_next_num)) 0 day9_inputs
+let find_prev_num list =
+  let rec aux lst =
+    let rec sub lst sub_lst =
+      match lst with l1 :: l2 :: tail -> sub (l2 :: tail) (sub_lst @ [ l2 - l1 ]) | _ -> sub_lst
+    in
 
-let execute' () = 0
+    if List.exists (fun x -> x <> 0) lst then
+      let prev_sub = sub lst [] in
+      let res = aux prev_sub in
+      let return = List.hd prev_sub - res in
+      return
+    else 0
+  in
+  List.hd list - aux list
+
+let execute () = List.fold_left (fun acc x -> acc + (parse_line x |> find_next_num)) 0 day9_inputs
+
+let execute' () =
+  List.fold_left (fun acc x -> acc + (parse_line x |> find_prev_num)) 0 day9_inputs
