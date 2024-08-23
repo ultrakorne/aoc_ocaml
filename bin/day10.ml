12e9e1e32be65c9eb7e2f6cf3ae3829d8f7f9a39
Samir H <ultrakorne@gmail.com>
2024-08-23 23:56:23 +0200

    working on day10

--- bin/day10.ml
+++ bin/day10.ml
@@ -8,30 +8,38 @@ module Matrix = Map.Make (struct
   let compare = compare
 end)
 
+(* return a list of (coord, char) as fst and snd the coord of the starting position 'S'*)
 let parse_input input =
-  let rec parse_row seq x y mtx_lst =
+  let rec parse_row seq x y mtx_lst start =
     match seq () with
-    | Seq.Nil -> mtx_lst
+    | Seq.Nil -> (mtx_lst, start)
     | Seq.Cons (s, tail) ->
+        let start_coord = if start <> None then start else if s = 'S' then Some { x; y } else None in
         let new_lst = ({ x; y }, s) :: mtx_lst in
-        parse_row tail x (y + 1) new_lst
+        parse_row tail x (y + 1) new_lst start_coord
   in
-  let rec aux line x mtx_lst =
+  let rec aux line x mtx_lst start =
     match line with
-    | [] -> mtx_lst
+    | [] -> (mtx_lst, start)
     | l :: tail ->
         let seq = String.to_seq l in
-        let row = parse_row seq x 0 mtx_lst in
-        aux tail (x + 1) (row @ mtx_lst)
+        let row, start_coord = parse_row seq x 0 mtx_lst start in
+        aux tail (x + 1) (row @ mtx_lst) start_coord
   in
-  aux input 0 []
+  aux input 0 [] None
 
-let matrix_list = parse_input day10_inputs
+let matrix_list, start = parse_input day10_inputs
 let matrix = Matrix.of_list matrix_list
 
 let execute () =
   let coord1 = Matrix.find { x = 2; y = 1 } matrix in
-  let () = Printf.printf "\nvalue %c" coord1 in
-  0
+  match start with
+  | Some start_coord ->
+      let () = Printf.printf "\nstart x:%d y:%d" start_coord.x start_coord.y in
+      let () = Printf.printf "\nvalue %c" coord1 in
+      0
+  | None ->
+      let () = Printf.printf "no start character S found" in
+      0
 
 let execute' () = 0
