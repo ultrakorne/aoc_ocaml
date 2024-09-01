04d947304d0c225897bb986df4cce419f2afd537
Samir H <ultrakorne@gmail.com>
2024-09-01 21:15:19 +0200

    day star 1 might be working

--- bin/day12.ml
+++ bin/day12.ml
@@ -1,6 +1,7 @@
 let day12_inputs = Utils.read_lines "data/advent_12_data.txt"
 
-type spring = { dmg_mask : int; op_mask : int; unkn_mask : int; length : int; damage_group : int list }
+type spring = { dmg_mask : int; op_mask : int; unkn_mask : int; length : int }
+type spring_line = { spring : spring; damage_group : int list }
 
 let binary_string_of_int n =
   let rec aux n acc = if n = 0 then acc else aux (n lsr 1) (string_of_int (n land 1) ^ acc) in
@@ -22,7 +23,6 @@ let parse_input input =
     else
       let new_mask =
         {
-          acc with
           dmg_mask = acc.dmg_mask lsl 1;
           op_mask = acc.op_mask lsl 1;
           unkn_mask = acc.unkn_mask lsl 1;
@@ -42,7 +42,8 @@ let parse_input input =
     let split = String.split_on_char ' ' line in
     let spring_input = List.hd split in
     let nums = List.tl split |> List.hd |> String.split_on_char ',' |> List.map int_of_string in
-    parse_spring { dmg_mask = 0; op_mask = 0; unkn_mask = 0; length = 0; damage_group = nums } spring_input 0
+    let spring_line = parse_spring { dmg_mask = 0; op_mask = 0; unkn_mask = 0; length = 0 } spring_input 0 in
+    { spring = spring_line; damage_group = nums }
   in
   let rec aux acc lines =
     match lines with
@@ -53,61 +54,78 @@ let parse_input input =
   in
   List.rev (aux [] input)
 
-let check_combinations spring =
-  let rec find_valid_combinations acc comb n shift shift_max =
-    if shift + n > shift_max then acc
+let get_subspring spring n =
+  let mask = set_bits n in
+  {
+    dmg_mask = spring.dmg_mask land mask;
+    op_mask = spring.op_mask land mask;
+    unkn_mask = spring.unkn_mask land mask;
+    length = n;
+  }
+
+let check_combinations spring_line =
+  let rec find_valid_combinations acc comb n shift spring =
+    if shift + n > spring.length then acc
     else
       let comb' = comb lsl shift in
       let dmg_masked = comb' lor spring.dmg_mask in
       (* checking the left part of the mask, if its the same as comb. -1 is used because we still want to make sure there is a space on the right *)
-        let check_comb = comb' lsr (Int.max 0 (shift -1)) in
-      let dmg_masked_check = dmg_masked lsr (Int.max 0 (shift -1)) in
-      
+      let check_comb = comb' lsr Int.max 0 (shift - 1) in
+      let dmg_masked_check = dmg_masked lsr Int.max 0 (shift - 1) in
+
       (* Printf.printf "\ndmg_masked %s " (binary_string_of_int dmg_masked);
-      Printf.printf "check_comb %s " (binary_string_of_int check_comb );
-      Printf.printf "dmg_masked_check  %s " (binary_string_of_int dmg_masked_check  ); *)
-      
+         Printf.printf "check_comb %s " (binary_string_of_int check_comb );
+         Printf.printf "dmg_masked_check  %s " (binary_string_of_int dmg_masked_check  ); *)
       let is_valid_comb_dmg = check_comb = dmg_masked_check in
 
       (*check that is not on the ., the op mask since those cannot have damage*)
       let op_masked = comb' land spring.op_mask in
       let is_valid_comb_op = op_masked = 0 in
-      Printf.printf "\n(%d/%d) comb %s is valid for dmg %b and valid op %b" 
-     shift shift_max
-      (binary_string_of_int comb') is_valid_comb_dmg is_valid_comb_op;
-      if is_valid_comb_dmg && is_valid_comb_op then 
-        find_valid_combinations ((comb', shift - 1)::acc) comb n (shift + 1) shift_max 
-      else
-        find_valid_combinations acc comb n (shift + 1) shift_max 
-
+      (* Printf.printf "\n(%d/%d) comb %s is valid for dmg %b and valid op %b" shift spring.length
+         (binary_string_of_int comb') is_valid_comb_dmg is_valid_comb_op; *)
+      if is_valid_comb_dmg && is_valid_comb_op then
+        find_valid_combinations ((comb', shift - 1) :: acc) comb n (shift + 1) spring
+      else find_valid_combinations acc comb n (shift + 1) spring
   in
 
-  let rec aux nums length =
+  let rec aux nums spring =
+    let rec traverse_comb acc combs rest_nums spring =
+      match combs with
+      | [] -> acc
+      | c :: tail ->
+          Printf.printf "\nTraversing combination %s" (binary_string_of_int (fst c));
+          let sub_spring = get_subspring spring (snd c) in
+          let valid = aux rest_nums sub_spring in
+          Printf.printf "\ncombination %s is valid %d " (binary_string_of_int (fst c)) valid;
+          traverse_comb (acc + valid) tail rest_nums spring
+    in
+
     (* TODO for every combs , we expand based on the remaining length*)
     match nums with
-    | [] -> 0
+    | [] -> 1
     | n :: rest ->
         let start = set_bits n in
-        Printf.printf "\n start find valid comb for n %d: %s" n (binary_string_of_int start);
-        let combs = find_valid_combinations [] start n 0 length in
-        List.iter (fun c -> Printf.printf "\n valid comb %s %d" (binary_string_of_int (fst c)) (snd c)) combs;
-        (* new lenght TODO*)
-        (*
-        *)
-        aux rest length
+        Printf.printf "\n start find valid comb for num %d: %s" n (binary_string_of_int start);
+        let combs = find_valid_combinations [] start n 0 spring in
+        List.iter (fun c -> Printf.printf "\n valid combinations %s %d" (binary_string_of_int (fst c)) (snd c)) combs;
+        if List.is_empty combs then 0
+        else
+          (* combs snd has the lenght of how much left there is for the rest of the nums *)
+          traverse_comb 0 combs rest spring
   in
-  aux spring.damage_group spring.length
+
+  aux spring_line.damage_group spring_line.spring
 
 let springs = parse_input day12_inputs
 
 let execute () =
   let first_spring = List.hd springs in
-  Printf.printf "dmg mask %d and op mask %d" first_spring.dmg_mask first_spring.op_mask;
+  Printf.printf "dmg mask %d and op mask %d" first_spring.spring.dmg_mask first_spring.spring.op_mask;
   Printf.printf "\ndmg mask %s and op mask %s, ? mask %s - length %d"
-    (binary_string_of_int first_spring.dmg_mask)
-    (binary_string_of_int first_spring.op_mask)
-    (binary_string_of_int first_spring.unkn_mask)
-    first_spring.length;
+    (binary_string_of_int first_spring.spring.dmg_mask)
+    (binary_string_of_int first_spring.spring.op_mask)
+    (binary_string_of_int first_spring.spring.unkn_mask)
+    first_spring.spring.length;
   check_combinations first_spring
 
 let execute' () = 0
